0f07361d2a4c603c383c0ae7dc6e3321229d9ef8
Victor <pvvx@mail.ru>
2025-12-13 16:41:09 +0300

    update GPIO CH592F

--- source/ch592/INA2282USB/src/common.h
+++ source/ch592/INA2282USB/src/common.h
@@ -22,8 +22,8 @@
 #define TESTS_DAC_STEP_ADC  0
 
 #define I2C_DEV_POWER GPIO_Pin_23 // PortB
-#define I2C_DEV_SDA   GPIO_Pin_12 // PortB
-#define I2C_DEV_SCL   GPIO_Pin_13 // PortB
+#define I2C_DEV_SDA   GPIO_Pin_14 // PortB
+#define I2C_DEV_SCL   GPIO_Pin_15 // PortB
 //#define GPIO_TEST     GPIO_Pin_7 // PortB
 
 #define DEBUG_USB       0 // =DEBUG debug print usb
